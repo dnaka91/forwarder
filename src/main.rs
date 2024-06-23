3e8ac9833a64dff73e16eda04cc5591e80d93d73
Dominik Nakamura <dnaka91@gmail.com>
2024-06-23 18:59:14 +0200

    chore: update to hyper 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,91 @@
 #![forbid(unsafe_code)]
 #![deny(rust_2018_idioms, clippy::all, clippy::pedantic)]
 #![warn(clippy::nursery)]
-#![allow(clippy::used_underscore_binding)]
+#![allow(clippy::redundant_pub_crate)]
 
-use std::{convert::Infallible, net::SocketAddr};
+use std::{net::SocketAddr, time::Duration};
 
+use http::{header, uri::PathAndQuery, Result, StatusCode, Uri};
+use http_body_util::Empty;
 use hyper::{
-    http::{uri::PathAndQuery, Result, Uri},
-    service::{make_service_fn, service_fn},
-    Body, Request, Response, Server, StatusCode,
+    body::{Bytes, Incoming},
+    service::service_fn,
+    Request, Response,
 };
+use hyper_util::{
+    rt::{TokioExecutor, TokioIo},
+    server::{conn::auto, graceful::GracefulShutdown},
+};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_shutdown::Shutdown;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
+    let listener = TcpListener::bind(&addr).await?;
 
-    let make_svc = make_service_fn(|_| async { Ok::<_, Infallible>(service_fn(forward)) });
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = auto::Builder::new(TokioExecutor::new());
+    let graceful = GracefulShutdown::new();
+    let shutdown = Shutdown::new()?;
 
-    println!("listening on {}", server.local_addr());
+    println!("listening on {addr}");
 
-    let server = server.with_graceful_shutdown(shutdown_signal());
+    loop {
+        tokio::select! {
+            conn = listener.accept() => {
+                handle_connection(&server, &graceful, conn);
+            }
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {e}");
+            () = shutdown.handle() => {
+                drop(listener);
+                eprintln!("shutdown signal received");
+                break;
+            }
+        }
     }
+
+    tokio::select! {
+        () = graceful.shutdown() => {
+            eprintln!("gracefully shutdown");
+        }
+        () = tokio::time::sleep(Duration::from_secs(10)) => {
+            eprintln!("shutdown timeout exceeded, stopping");
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_connection(
+    server: &auto::Builder<TokioExecutor>,
+    graceful: &GracefulShutdown,
+    conn: std::io::Result<(TcpStream, SocketAddr)>,
+) {
+    let (stream, _) = match conn {
+        Ok(conn) => conn,
+        Err(e) => {
+            eprintln!("accept error: {e}");
+            return;
+        }
+    };
+
+    let stream = TokioIo::new(Box::pin(stream));
+    let conn = server.serve_connection_with_upgrades(stream, service_fn(forward));
+    let conn = graceful.watch(conn.into_owned());
+
+    tokio::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("connection error: {e}");
+        }
+    });
 }
 
 #[allow(clippy::unused_async)]
-async fn forward(req: Request<Body>) -> Result<Response<Body>> {
+async fn forward(req: Request<Incoming>) -> Result<Response<Empty<Bytes>>> {
     Response::builder()
         .status(StatusCode::FOUND)
         .header(
-            "Location",
+            header::LOCATION,
             Uri::builder()
                 .scheme("https")
                 .authority("home.dnaka91.rocks")
@@ -43,13 +96,5 @@ async fn forward(req: Request<Body>) -> Result<Response<Body>> {
                     |u| u.to_string(),
                 ),
         )
-        .body(Body::empty())
-}
-
-async fn shutdown_signal() {
-    if tokio::signal::ctrl_c().await.is_err() {
-        eprintln!("failed to install CTRL+C signal handler");
-    }
-
-    println!("shutting down");
+        .body(Empty::new())
 }
